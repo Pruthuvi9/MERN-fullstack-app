873140c7eebfb9e08cae7c17170be39a6056aae1
Pruthuvi Fernando <pruthuvi.fernando@gmail.com>
2024-01-02 15:31:04 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+
+import Users from "./user/pages/Users.js";
+import NewPlace from "./places/pages/NewPlace.js";
+
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Users />
+                </Route>
+                <Route path="/places/new" exact>
+                    <NewPlace />
+                </Route>
+                <Redirect to="/" />
+            </Switch>
+        </Router>
+    );
+};
+
+export default App;
