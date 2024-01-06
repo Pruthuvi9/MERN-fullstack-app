5855bf1667fef14a4cc00551312b25e4a8b59c86
Pruthuvi Fernando <pruthuvi.fernando@gmail.com>
2024-01-06 14:51:10 +0530

    add custom form input element

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -2,6 +2,48 @@ import React, { useRef, useEffect } from "react";
 
 import "./Map.css";
 
+//import google maps
+((g) => {
+  var h,
+    a,
+    k,
+    p = "The Google Maps JavaScript API",
+    c = "google",
+    l = "importLibrary",
+    q = "__ib__",
+    m = document,
+    b = window;
+  b = b[c] || (b[c] = {});
+  var d = b.maps || (b.maps = {}),
+    r = new Set(),
+    e = new URLSearchParams(),
+    u = () =>
+      h ||
+      (h = new Promise(async (f, n) => {
+        await (a = m.createElement("script"));
+        e.set("libraries", [...r] + "");
+        for (k in g)
+          e.set(
+            k.replace(/[A-Z]/g, (t) => "_" + t[0].toLowerCase()),
+            g[k]
+          );
+        e.set("callback", c + ".maps." + q);
+        a.src = `https://maps.${c}apis.com/maps/api/js?` + e;
+        d[q] = f;
+        a.onerror = () => (h = n(Error(p + " could not load.")));
+        a.nonce = m.querySelector("script[nonce]")?.nonce || "";
+        m.head.append(a);
+      }));
+  d[l]
+    ? console.warn(p + " only loads once. Ignoring:", g)
+    : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n)));
+})({
+  key: `${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`,
+  v: "weekly",
+  // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+  // Add other bootstrap parameters as needed, using camel case.
+});
+
 const Map = (props) => {
   const mapRef = useRef();
 
@@ -18,7 +60,7 @@ const Map = (props) => {
       let map = new Map(mapRef.current, {
         zoom: zoom,
         center: center,
-        mapId: "a56457eb3e3c44ed",
+        mapId: `${process.env.REACT_APP_GOOGLE_MAPS_MAP_ID}`,
       });
 
       new AdvancedMarkerElement({
