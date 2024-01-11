3a82a02e760cf101827c92f74dd21981fadfdf24
Pruthuvi Fernando <pruthuvi.fernando@gmail.com>
2024-01-11 15:50:42 +0530

    add context, authenticated and unauthenticated routes, and complete the React front end

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import {
     VALIDATOR_EMAIL,
     VALIDATOR_MINLENGTH,
+    VALIDATOR_REQUIRE,
 } from "../../shared/util/validators";
 import { useForm } from "../../shared/hooks/form-hook";
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import Card from "../../shared/components/UIElements/Card";
+import { AuthContext } from "../../shared/context/auth-context";
 import "./Auth.css";
 
-const Auth = () => {
-    const [formState, inputHandler] = useForm(
+const Auth = (props) => {
+    const auth = useContext(AuthContext);
+
+    const [isLoginMode, setIsLoginMode] = useState(true);
+
+    const [formState, inputHandler, setFormData] = useForm(
         {
+            // name: {
+            //     value: "",
+            //     isValid: false,
+            // },
             email: {
                 value: "",
                 isValid: false,
@@ -25,9 +35,35 @@ const Auth = () => {
         false
     );
 
+    const switchModeHandler = () => {
+        if (!isLoginMode) {
+            setFormData(
+                {
+                    ...formState.inputs,
+                    name: undefined,
+                },
+                formState.inputs.email.isValid &&
+                    formState.inputs.password.isValid
+            );
+        } else {
+            setFormData(
+                {
+                    ...formState.inputs,
+                    name: {
+                        value: "",
+                        isValid: false,
+                    },
+                },
+                false
+            );
+        }
+        setIsLoginMode((prevMode) => !prevMode);
+    };
+
     const authSubmitHandler = (event) => {
         event.preventDefault();
         console.log(formState.inputs);
+        auth.login();
     };
 
     return (
@@ -35,6 +71,17 @@ const Auth = () => {
             <h2>Login Required</h2>
             <hr />
             <form onSubmit={authSubmitHandler}>
+                {!isLoginMode && (
+                    <Input
+                        id="name"
+                        element="input"
+                        type="text"
+                        label="Your Name"
+                        validators={[VALIDATOR_REQUIRE()]}
+                        errorText="Please enter your name."
+                        onInput={inputHandler}
+                    />
+                )}
                 <Input
                     id="email"
                     element="input"
@@ -54,7 +101,10 @@ const Auth = () => {
                     onInput={inputHandler}
                 />
                 <Button type="submit" disabled={!formState.isValid}>
-                    LOGIN
+                    {isLoginMode ? "LOGIN" : "SIGNUP"}
+                </Button>
+                <Button inverse onClick={switchModeHandler}>
+                    SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
                 </Button>
             </form>
         </Card>
